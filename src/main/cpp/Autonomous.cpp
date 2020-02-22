e5bf1db23b199bef924f9f7a35d81ce830d43072
cuthbertnibbles <cuthbert.nibbles@gmail.com>
2020-02-21 20:08:53 -0500

    Created Files

---
+++ src/main/cpp/Autonomous.cpp
@@ -0,0 +1,2 @@
+/**
+ * Contains code for running fully autonomous functions, called during Au
---
+++ src/main/include/Autonomous.h
@@ -0,0 +1,20 @@
+/**
+ * Contains all the routines, watchdogs and persistent variables for making the robot do things on its own.
+ */
+
+class Autonomous
+{
+private:
+    /* data */
+public:
+    Autonomous(/* args */);
+    ~Autonomous();
+};
+
+Autonomous::Autonomous(/* args */)
+{
+}
+
+Autonomous::~Autonomous()
+{
+}
