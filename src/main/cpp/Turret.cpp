da6e92f41db72832fd1c5ab1e73fda6ea8735d4d
cuthbertnibbles <cuthbert.nibbles@gmail.com>
2020-02-27 02:02:09 -0500

    Reworked Turret Position Control Framework (1/n)

--- src/main/cpp/Turret.cpp
+++ src/main/cpp/Turret.cpp
@@ -21,11 +21,12 @@ Turret::Turret(int CANID) {
 
     //Set PIDs
     this->m_pTurretServo->SelectProfileSlot(0, 0);
-    this->m_pTurretServo->Config_kF(0, 0.3, 10); //LAST: 0.3
-    this->m_pTurretServo->Config_kP(0, 3.2, 10); //LAST: 1.6
+    this->m_pTurretServo->Config_kF(0, 0.0, 10); //LAST: 0.0
+    this->m_pTurretServo->Config_kP(0, 0.0, 10); //LAST: 0.0
     this->m_pTurretServo->Config_kI(0, 0.0, 10); //LAST: 0.0
-    this->m_pTurretServo->Config_kD(0, 6.0, 10); //LAST: 0.0
+    this->m_pTurretServo->Config_kD(0, 0.0, 10); //LAST: 0.0
 
+    //Motion Profile Properties in case we get an encoder
     this->m_pTurretServo->ConfigMotionCruiseVelocity(100, 10);
     this->m_pTurretServo->ConfigMotionAcceleration(100, 0);
 
@@ -35,45 +36,132 @@ Turret::Turret(int CANID) {
 }
 
 /**
- * If the turret is within a preset error margin from the target, returns true
+ * Pull the latest center-of-target pixel from NetworkTables
  */
-bool Turret::GetLockState() {
-    return (this->GetFOVTargetXAngle() > this->pAllowableTurretError);
+double Turret::GetRawXPixel() {
+    return frc::SmartDashboard::GetNumber("visionTargetXPos", -1);
 }
 
-//Returns the angle of the target relative to the turret in degrees
-double Turret::GetFOVTargetXAngle() {
-    this->pCameraTargetFOVXAngle = this->pCameraXFOV * this->pCameraTargetFOVXAngle;
-    return this->pCameraTargetFOVXAngle;
-}
-//Converts from FOVTargetFactor to FOVTargetAngle
-double Turret::GetFOVTargetAngle(double FOVTargetFactor) {
-    this->pCameraTargetFOVXAngle = this->pCameraXFOV * FOVTargetFactor;
-    return this->pCameraTargetFOVXAngle;
+/**
+ * Get the latest target pixel, then calculate the ratio of its position on the camera frame
+ */
+double Turret::GetFOVXFactor() {
+    double rawXPixel = this->GetRawXPixel();
+    return rawXPixel * this->mCameraFOVXResRatio - 1;
 }
 
-//Returns the angle of the target relative to the turret as a factor of the camera's FOV (-1 to +1)
-double Turret::GetFOVTargetFactor() {
-    this->pCameraTargetFOVXFactor = this->pCameraFOVResRatio * this->pCameraTargetHPos;
-    return this->pCameraTargetFOVXFactor;
-}
-//Converts from hPos to FOVTargetFactor
-double Turret::GetFOVTargetFactor(double hPos) {
-    this->pCameraTargetFOVXFactor = this->pCameraFOVResRatio * hPos;
-    return this->pCameraTargetFOVXFactor;
+
+/**
+ * Use the provided pixel to calculate the ratio of its position on the camera frame
+ */
+double Turret::GetFOVXFactor(double rawXPixel) {
+    return rawXPixel * this->mCameraFOVXResRatio - 1;
 }
 
-//Returns the angle of the target relative to the frame
-double Turret::GetFrameTargetAngle() {
-    /**
-     * TODO:
-     */
+/**
+ * Use the latest pixel data to calculate the angle of the target relative to the camera
+ */
+double Turret::GetFOVXAngle() {
+    double xFactor = this->GetFOVXFactor();
+    return xFactor * this->mCameraXFOV;
 }
 
+/**
+ * Use the provided pixel data to calculate the angle of the target relative to the camera
+ */
+double Turret::GetFOVXAngle(double xFactor) {
+    return xFactor * this->mCameraXFOV;
+}
+
+/**
+ * Calculates the turret's angle from frame center
+ */
+double Turret::GetTurretFrameAngle() {
+    return this->m_pTurretServo->GetSelectedSensorPosition() / this->mEncoderTicksPerDegree;
+}
+
+
+/**
+ * Combines the turret-frame and target-turret angles
+ */
+double Turret::GetTargetFrameAngle() {
+    return this->GetTurretFrameAngle() + this->GetFOVXAngle();
+}
+
+
+/**
+ * Returns TRUE when GetFOVXAngle is within tolerable error margin
+ */
+bool Turret::GetTurretLocked() {
+    return (this->GetFOVXAngle < this->mAllowableTurretError);
+}
+
+/**
+ * Checks the limit switch sticky flags on the servo and the Homed object
+ * If any of them are awry, flag the system as not-homed and return false
+ * Otherwise, return true
+ */
+bool Turret::GetHomed() {
+    //If homing, let RunAutoHome handle this
+    if (m_Homing) {
+        this->RunAutoHome();
+        return this->m_Homed;
+    }
+
+    //If we're lost, find the way
+    if (!this->m_Homed) {
+        this->RunAutoHome();
+        return this->m_Homed;
+    }
+    
+    //If not homing, check to see if something has gone wrong
+    //Get Sticky Faults from Servo Talon
+    ctre::phoenix::motorcontrol::StickyFaults stickies;
+    this->m_pTurretServo->GetStickyFaults(stickies);
+
+    //If either of the limit switches have been hit, we're lost
+    if (stickies.ForwardLimitSwitch || stickies.ReverseLimitSwitch) {
+        //Set the system to lost
+        this->m_Homed = false;
+        //Start working on a fix
+        this->RunAutoHome();
+        //Tell the caller that we're lost (RunAutoHome should have set to false)
+        return this->m_Homed;
+    }
+
+    //If none of the above returns have run, we should be in the clear!
+    return this->m_Homed;
+    
+}
+
+
+/**
+ * If lost, run the mechanism towards the homing limit switch
+ * If at the homing limit switch, set the encoder and set faults to be cleared
+ * TODO: Finish this?
+ */
+void Turret::RunAutoHome() {
+    
+}
+
+
+/**
+ * If turret is homed, converts from target angle to target ticks, then pushes to Talon servo
+ * If not, runs homing
+ */
+void Turret::SetTurretAngle(double requestAngle) {
+
+    if (this->GetHomed()) {
+
+    }
+    double absoluteAngle = requestAngle + 90 + mHomeFrameOffset;
+    double absolutePosition = absoluteAngle * this->mEncoderTicksPerDegree;
+    this->m_pTurretServo->Set(ControlMode::MotionMagic, absolutePosition);
+}
+
+/**
+ * Sets a percentoutput on the servo motor
+ */
 void Turret::SetTurretPower(double power) {
     this->m_pTurretServo->Set(ControlMode::PercentOutput, power);
-}
-
-void Turret::SetTurretAngle(double angle) {
-    
 }
--- src/main/include/Turret.h
+++ src/main/include/Turret.h
@@ -3,62 +3,84 @@
  * See Shooter.h for the shooter wheel code
  */
 
+#pragma once
+
 #include "ctre/Phoenix.h"
+#include <frc/smartdashboard/SmartDashboard.h>
 
 class Turret {
     public:
     Turret(int CANID);
     ~Turret();
 
-    //Returns true when turret is aimed at target
-    bool GetLockState();
+    //Returns the number of pixels of the target from camera left
+    double GetRawXPixel();
 
-    //Returns the angle of the target relative to the turret in degrees
-    double GetFOVTargetXAngle();
+    //Returns a signed ratio of horizontal pixels from center to horizontal pixels
+    double GetFOVXFactor();
+    double GetFOVXFactor(double rawXPixel);
 
-    //Converts from FOVTargetFactor to FOVTargetAngle
-    double GetFOVTargetAngle(double FOVTargetFactor);
+    //Returns the angle (in degrees) of the target relative to the camera
+    double GetFOVXAngle();
+    double GetFOVXAngle(double xFactor);
 
-    //Returns the angle of the target relative to the turret as a factor of the camera's FOV (-1 to +1)
-    double GetFOVTargetFactor();
-    //Converts from hPos to FOVTargetFactor and sets a new target hPos
-    double GetFOVTargetFactor(double hPos);
+    //Returns the current measured orientation of the turret from the frame in degrees
+    double GetTurretFrameAngle();
 
-    //Returns the angle of the target relative to the frame
-    double GetFrameTargetAngle();
+    //Returns the current measured angle of the target from the frame in degrees
+    double GetTargetFrameAngle();
+
+    //Returns true when the turret is within allowable shooting error margin
+    bool GetTurretLocked();
+
+    //If homed, return true. If not, fires "RunAutoHome"
+    bool GetHomed();
+
+
+    //Servo to an angle in degrees from center (-90 <-> +90)
+    void SetTurretAngle(double requestAngle);
+
+    //Runs the turret into the home switch
+    void RunAutoHome();
+
+
 
     //For testing (or backup), set the power to the turret motor.
     void SetTurretPower(double power);
 
-    //Servo to an angle from center in degrees
-    void SetTurretAngle(double angle);
-    void SetTurretTarget(double position);
+
 
     private:
     //Motor that rotates the turret
     WPI_TalonSRX* m_pTurretServo;
 
     //Last known horizontal pixel of the target (initialize at center)
-    double pCameraTargetHPos = this->pCameraXRes / 2;
-    //Last known horizontal angle of the target
-    double pCameraTargetFOVXAngle = 0;
-    //Last known horizontal angle factor between camera FOV and target
-    double pCameraTargetFOVXFactor = 0;
+    double mCameraTargetHPos = this->mCameraXRes / 2;
+
+    //Whether the turret is homed
+    bool m_Homed = false;
+    //Whether an attempt is being made to change that
+    bool m_Homing = false;
 
 //Constants
     //Camera Resolution
-    const double pCameraXRes = 640;
-    const double pCameraYRes = 480;
+    const double mCameraXRes = 640;
+    const double mCameraYRes = 480;
     //Camera FOV
-    const double pCameraXFOV = 61;
-    const double pCameraYFOV = 34.3;
+    const double mCameraXFOV = 61;
+    const double mCameraYFOV = 34.3;
+
     //Number of encoder ticks per turret degree 
-    const double pEncoderTicksPerDegree = (360 * 48 * 33.02) / 2.8575; //Was 2940;
+    const double mEncoderTicksPerDegree = ((4096 / 360) * 48 * 33.02) / 2.8575; //Was 2940; is ((ticksperrev / degrees) * gearbox * ringdiameter) / piniondiameter
+
     //Maximum allowable Error between turret and target in degrees
-    const double pAllowableTurretError = 5;
+    const double mAllowableTurretError = 5;
+
+    //Offset between homing limit switch and turret zero (pointing directly backwards) in degrees
+    const double mHomeFrameOffset = 100;
 
     //Calculated Constants
-    const double pCameraFOVResRatio = 2 / this->pCameraXRes;
+    const double mCameraFOVXResRatio = 2 / this->mCameraXRes;
 
     
 
